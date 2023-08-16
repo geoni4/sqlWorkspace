7a18505e80f3db81e619e1280c4dc2819ed3d69f
geoni4 <seralove4@gmail.com>
2023-08-16 12:16:44 +0900

    commiting

--- 230810.sql
+++ 230810.sql
@@ -1,5 +1,6 @@
 select * from (
     select rownum as rnum, m.* from (
-        select * from mvc_board order by bgroup desc, bstep asc)
-    m where rnum < 20)
-where rnum >=10;
+        select * from mvc_board order by bgroup desc, bstep asc
+    ) m where rownum <= 20
+)
+where rnum >10;
