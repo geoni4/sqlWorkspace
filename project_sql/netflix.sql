5999d303df926322214ca3c3ae10ec6d6a8af203
geoni4 <seralove4@gmail.com>
2023-08-22 03:07:22 +0900

    member_id column 추가로 email이 일반 email로.. member_id -> email_social

--- project_sql/netflix.sql
+++ project_sql/netflix.sql
@@ -4,31 +4,40 @@ drop table NETFLIX_MEMBER cascade constraints;
 
 
 create table NETFLIX_MEMBER(
-    member_email varchar2(50) primary key,
+    member_id varchar2(60) primary key,
+    member_email varchar2(50),
     member_password varchar2(60) not null,
     member_card_number char(16),
-    member_membership_grade varchar2(10) not null,
+    member_membership_no number not null,
     member_social varchar2(10) default 'NONE'
 );
 
 insert into netflix_member values(
     'tatelulove4@naver.com',
-    'Dldnjsrjs4$4',
+    'tatelulove4@naver.com',
+    '$2a$10$7DxPpaKDw/wcVUT0//J.Aen1gkroamLzBhZOMl2YLy8GUIIdZvVTy',
     null,
-    'basic',
+    1,
     'NONE'
 );
-
+insert into netflix_member values(
+    'tatelulove4@naver.com_KAKAO',
+    'tatelulove4@naver.com',
+    '$2a$10$7DxPpaKDw/wcVUT0//J.Aen1gkroamLzBhZOMl2YLy8GUIIdZvVTy',
+    null,
+    1,
+    'KAKAO'
+);
 ----------------------------------------------------------
 
 drop table NETFLIX_AUTH cascade constraints;
 
 create table NETFLIX_AUTH(
-    auth_member_email varchar2(50) not null,
+    auth_member_id varchar2(60) not null,
     auth_member_authority varchar2(10) not null,
 
-    constraint ix_netflix_auth UNIQUE(auth_member_email, auth_member_authority),
-    constraint fk_auth_member_email foreign key(auth_member_email) references NETFLIX_MEMBER(member_email)
+    constraint ix_netflix_auth UNIQUE(auth_member_id, auth_member_authority),
+    constraint fk_auth_member_id foreign key(auth_member_id) references NETFLIX_MEMBER(member_id)
       on delete cascade
 );
 
@@ -37,16 +46,26 @@ insert into netflix_auth values(
     'ROLE_USER'
 );
 
+insert into netflix_auth values(
+    'tatelulove4@naver.com',
+    'ROLE_ADMIN'
+);
+
+insert into netflix_auth values(
+    'tatelulove4@naver.com_KAKAO',
+    'ROLE_USER'
+);
+
 ---------------------------------------------------------
 
 drop table NETFLIX_MEMBER_PROFILE cascade constraints;
 
 create table NETFLIX_MEMBER_PROFILE(
-    member_profile_email varchar2(50) not null,
+    member_profile_member_id varchar2(60) not null,
     member_profile_name varchar2(50) not null,
     
-    constraint fk_member_profile_email
-        foreign key (member_profile_email) references NETFLIX_MEMBER(member_email)
+    constraint fk_member_profile_member_id
+        foreign key (member_profile_member_id) references NETFLIX_MEMBER(member_id)
         on delete cascade
 );
 
@@ -55,13 +74,13 @@ create table NETFLIX_MEMBER_PROFILE(
 drop table NETFLIX_ORDER cascade constraints;
 
 create table NETFLIX_ORDER(
-    order_id number(8) primary key,
-    order_member_email varchar2(50) not null,
+    order_id number primary key,
+    order_member_id varchar2(60) not null,
     order_member_card_number char(16),
     order_start_date date default sysdate,
     
-    constraint fk_order_member_email
-        foreign key (order_member_email) references NETFLIX_MEMBER(member_email)
+    constraint fk_order_member_id
+        foreign key (order_member_id) references NETFLIX_MEMBER(member_id)
         on delete cascade
 );
 
@@ -70,18 +89,27 @@ create table NETFLIX_ORDER(
 drop table NETFLIX_MEMBERSHIP cascade constraints;
 
 create table NETFLIX_MEMBERSHIP(
-    member_id number primary key,
+    membership_no number primary key,
     membership_grade varchar2(10) unique
 );
 insert into netflix_membership values(
-    '1',
+    1,
     'basic'
+);
+
+insert into netflix_membership values(
+    2,
+    'standard'
+);
+insert into netflix_membership values(
+    3,
+    'premium'
 );
 
 
 alter table NETFLIX_MEMBER
-add constraint fk_member_membership_grade
-foreign key (member_membership_grade) references NETFLIX_MEMBERSHIP(membership_grade)
+add constraint fk_member_membership_no
+foreign key (member_membership_no) references NETFLIX_MEMBERSHIP(membership_no)
 on delete cascade;
 
 
@@ -111,13 +139,13 @@ create table NETFLIX_MOVIE(
 drop table NETFLIX_QA_BOARD cascade constraints;
 
 create table NETFLIX_QA_BOARD(
-    QA_BOARD_NO number primary key,
-    QA_BOARD_WRITE varchar2(50), --member_profile_name fk
-    QA_BOARD_TITLE varchar2(200),
-    QA_BOARD_CONTENT varchar2(500),
-    QA_BOARD_CREATED_DATE date default sysdate,
-    QA_BOARD_MODIFIED_DATE date default sysdate,
-    QA_BOARD_HIT number
+    qa_board_no number primary key,
+    qa_board_write varchar2(50), --member_profile_name fk
+    qa_board_title varchar2(200),
+    qa_board_content varchar2(500),
+    qa_board_created_date date default sysdate,
+    qa_board_modified_date date default sysdate,
+    qa_board_hit number
 );
 
 
@@ -126,28 +154,28 @@ create table NETFLIX_QA_BOARD(
 drop table NETFLIX_FAVORITES cascade constraints;
 
 CREATE TABLE NETFLIX_FAVORITES(
-    favorites_member_email varchar2(50) not null,
-    FAVORITES_MEMBER_PROFILE_NAME VARCHAR2(50) NOT NULL,
-    FAVORITES_MOVIE_ID NUMBER NOT NULL,
-    constraint ix_favorites_member UNIQUE(favorites_member_email, favorites_member_profile_name),
-    CONSTRAINT FK_FAV_MOVIE
-        FOREIGN KEY (FAVORITES_MOVIE_ID) REFERENCES NETFLIX_MOVIE(MOVIE_ID)
+    favorites_member_id varchar2(60) not null,
+    favorites_member_profile_name VARCHAR2(50) NOT NULL,
+    favorites_movie_id NUMBER NOT NULL,
+    constraint ix_favorites_member UNIQUE(favorites_member_id, favorites_member_profile_name),
+    CONSTRAINT fk_fav_movie
+        FOREIGN KEY (favorites_movie_id) REFERENCES NETFLIX_MOVIE(movie_id)
         ON DELETE CASCADE
 );
 
 --------------------------------
 
-drop table NETFLIX_SOCIAL_ACCOUNT cascade constraints;
+-- drop table NETFLIX_SOCIAL_ACCOUNT cascade constraints;
 
-create table NETFLIX_SOCIAL_ACCOUNT(
-    social_account_member_email varchar2(50), -- fk
-    social_account_member_social varchar2(10), -- fk
+-- create table NETFLIX_SOCIAL_ACCOUNT(
+--     social_account_member_email varchar2(50), -- fk
+--     social_account_member_social varchar2(10), -- fk
     
-     constraint ix_netflix_social_account UNIQUE(social_account_member_email, social_account_member_social),
-    constraint fk_social_account_member_email
-        foreign key (social_account_member_email) references NETFLIX_MEMBER(member_email)
-        on delete cascade
-);
+--      constraint ix_netflix_social_account UNIQUE(social_account_member_email, social_account_member_social),
+--     constraint fk_social_account_member_email
+--         foreign key (social_account_member_email) references NETFLIX_MEMBER(member_email)
+--         on delete cascade
+-- );
 
 --------------------------------
 
@@ -155,17 +183,23 @@ drop table NETFLIX_SOCIAL cascade constraints;
 
 create table NETFLIX_SOCIAL(
     social_no number primary key,
-    social_publisher varchar2(10) not null unique
+    social_provider varchar2(10) not null unique
 );
 
-alter table netflix_social_account
-    add constraint fk_SA_member_social
-    foreign key (social_account_member_social) references NETFLIX_SOCIAL(social_publisher)
-on delete cascade;
+-- alter table netflix_social_account
+--     add constraint fk_SA_member_social
+--     foreign key (social_account_member_social) references NETFLIX_SOCIAL(social_provider)
+-- on delete cascade;
 
 insert into netflix_social values( (select nvl(max(social_no),0)+1 from netflix_social), 'NONE');
 insert into netflix_social values( (select nvl(max(social_no),0)+1 from netflix_social), 'KAKAO');    
 insert into netflix_social values( (select nvl(max(social_no),0)+1 from netflix_social), 'GOOGLE');
+
+alter table netflix_member
+    add constraint fk_member_social
+    foreign key (member_social) references NETFLIX_SOCIAL(social_provider)
+on delete cascade;
+
 
 
 
@@ -197,7 +231,7 @@ select * from NETFLIX_MEMBERSHIP;
 select * from NETFLIX_MOVIE;
 select * from NETFLIX_QA_BOARD;
 select * from NETFLIX_FAVORITES;
-select * from NETFLIX_SOCIAL_ACCOUNT;
+-- select * from NETFLIX_SOCIAL_ACCOUNT;
 select * from netflix_social;
 
 
@@ -212,10 +246,11 @@ commit;
 
 
 select * from netflix_member mb, netflix_auth au
-    where mb.member_email = au.auth_member_email and mb.member_email = 'user@example.com';
+    where mb.member_id = au.auth_member_id and mb.member_id = 'tatelulove4@naver.com_KAKAO';
 
 
 
 --------------------
 
 delete from netflix_member where member_email = 'user@example.com';
+delete from netflix_member where member_email = 'tatelulove4@naver.com';
